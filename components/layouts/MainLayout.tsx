a9a8388d613ffb6af90f86069efbe88881690e7c
Fabrizio Condori <fabrizio.condori@gitlab.com>
2022-03-06 21:52:23 -0500

    first commit

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import Head from "next/head";
+import { NavBar } from "../NavBar";
+import styles from "./MainLayout.module.css";
+
+export const MainLayout: FC = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Fabrizio</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
